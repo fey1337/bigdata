cb19231a4cfccc9ffa4575b7db0f8e6e5cdc0b9d
feiko <feiko.roeben@gmail.com>
2020-10-24 11:43:24 +0200

    import and finish task

---
+++ 3-1-hadoop_moviedata/ratings_breakdown.py
@@ -0,0 +1,23 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+
+class RatingsBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        (user_id, movie_id, rating, timestamp) = line.split('\t')
+        yield rating, 1
+        pass
+
+    def reducer_count_ratings(self, movie_id, count):   # (self, movie_id, count):
+        yield movie_id, sum(count)
+        pass
+
+
+if __name__ == '__main__':
+    RatingsBreakdown.run()
